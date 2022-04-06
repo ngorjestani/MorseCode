f220c15f0b2eb62d9d4bcdeac894be71872a13d0
Nick Gorjestani <nickg2794@hotmail.com>
2022-04-05 20:38:18 -0500

    Initial commit

---
+++ MorseCode/MorseCode/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MorseCode
+{
+    public partial class MainPage : ContentPage
+    {
+        private StringBuilder _morse;
+        private StringBuilder _displayString;
+        public MainPage()
+        {
+            _morse = new StringBuilder();
+            _displayString = new StringBuilder();
+            InitializeComponent();
+        }
+
+        public void AddToMorse(char m) => _morse.Append(m);
+
+        public char GetLetterForMorseCode() => Morse.MorseCoder(_morse.ToString());
+
+        public void OnDotButtonClicked() => AddToMorse('.');
+
+        public void OnDashButtonClicked() => AddToMorse('-');
+
+        public void OnSpaceButtonClicked()
+        {
+            if (_morse.Length == 0) _displayString.Append(' ');
+            else _displayString.Append(GetLetterForMorseCode());
+        }
+    }
+}
